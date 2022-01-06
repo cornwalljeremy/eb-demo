8234ce8527b6305633da137091843f5bbed891a9
Jeremy Cornwall <cornwall.jeremy@gmail.com>
2022-01-05 18:16:24 -0700

    another get command

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const { PORT = 3000 } = process.env;
 console.log('PORT', PORT);
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hola Mundo')
+});
+
+app.get('/users', (req, res) => {
+  res.send('Hello Users')
 });
 
 app.listen(PORT, () => {
